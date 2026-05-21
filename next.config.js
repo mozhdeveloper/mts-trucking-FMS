8541d8c9b4ef9f1b802b04806ec90ffb96400aa9
jcuady <jcuady@gmail.com>
2026-05-21 14:17:49 +0800

    fix: redirect webpack cache outside OneDrive to prevent cache corruption

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
+const path = require("path");
+const os = require("os");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -6,6 +9,16 @@ const nextConfig = {
       { protocol: "https", hostname: "images.unsplash.com" },
       { protocol: "https", hostname: "api.dicebear.com" },
     ],
+  },
+  webpack: (config, { dev }) => {
+    if (dev) {
+      config.cache = {
+        type: "filesystem",
+        cacheDirectory: path.join(os.tmpdir(), "mts-trucking-webpack-cache"),
+        buildDependencies: { config: [__filename] },
+      };
+    }
+    return config;
   },
 };
 module.exports = nextConfig;
